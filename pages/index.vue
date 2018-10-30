37d25c9bb1e934bcbd5bddf3d702e24aa9f05d4e
rama11996 <ramanathanpri@gmail.com>
2018-10-30 13:18:43 +0530

    Added all components into pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,40 @@
 <template lang="pug">
 .page
+  Appheader
+  Hero
+  About
+  Speakers
+  Agenda
+  SignUp
+  Sponsors
+  Tickets
   Register
+  AppFooter
 </template>
 
 <script>
-import Register from '@/components/Register'
+import Appheader from '@/components/sections/Appheader'
+import Hero from '@/components/sections/Hero'
+import About from '@/components/sections/About'
+import Speakers from '@/components/sections/Speakers'
+import Agenda from '@/components/sections/Agenda'
+import SignUp from '@/components/sections/SignUp'
+import Sponsors from '@/components/sections/Sponsors'
+import Tickets from '@/components/sections/Tickets'
+import Register from '@/components/sections/Register'
+import AppFooter from '@/components/sections/AppFooter'
 export default {
   components: {
-    Register
+  Appheader,
+  Hero,
+  About,
+  Speakers,
+  Agenda,
+  SignUp,
+  Sponsors,
+  Tickets,
+  Register,
+  AppFooter
   }
 }
 </script>
