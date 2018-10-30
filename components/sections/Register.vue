a7d606a259e097deddb14d2d75a29082722abeec
manojjonam <manoj.jonam.boxer@gmail.com>
2018-10-30 16:20:37 +0530

    Add Register component

--- components/sections/Register.vue
+++ components/sections/Register.vue
@@ -1,7 +1,10 @@
 <template lang="pug">
 section.register
   .container
-    h2 register
+    .content
+      h2 Are you ready to learn,connect get inspired and have fun?
+      p Event talk is the place for you. Resgister now and grab your seat Hurry , limited seat available
+      button.btn.btn-primary Resgister Now
 </template>
 <script>
 export default {
@@ -14,5 +17,18 @@ export default {
 section.register
   background: $white
   color: $black
+  .container
+    .content  
+      text-align: center
+      h2
+        width: 65%
+        margin: 0 auto
+        margin-top: $space*1.5
+        padding: $space*3
+      p
+        margin: 0 auto
+        width: 80%
+      button
+        margin: $space*3
 </style>
 
