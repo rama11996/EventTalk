cc941c7684c3ecc6d228a43b736fa051e8b1fa87
rama11996 <ramanathanpri@gmail.com>
2018-10-30 16:27:26 +0530

    Add about section

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,11 +1,35 @@
 <template lang="pug">
 section.about
   .container
-    h2 About
+    .content
+      h2 About the Event
+      p Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock
+    ul.items
+      li.item(v-for="(a, i) in about", :key="i")
+        .thumb
+          img(:src="a.image", alt="thumb")
+        .item-content
+          h3 {{ a.title }}
+          p {{ a.content }}
 </template>
 <script>
 export default {
-  
+  data () {
+    return {
+      about: [
+        {
+          title: 'Guidelines for paper submissions',
+          content: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going',
+          image: 'images/about1.png'
+        },
+        {
+          title: 'Activites and Structure',
+          content: 'All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.',
+          image: 'images/about2.png'
+        }
+      ]
+    }
+  }
 }
 </script>
 <style lang="sass" scoped>
@@ -14,5 +38,20 @@ export default {
 section.about
   background: $white
   color: $black
+  .content
+    text-align: center
+    padding: $space*3
+p
+  color: $neutral-light
+  margin-top: $space*2
+  line-height: 2rem
+ 
+
+.item
+  @include flex
+  &:nth-child(odd)
+    flex-direction: row-reverse
+  .thumb, .item-content
+    flex: 1
 </style>
 
